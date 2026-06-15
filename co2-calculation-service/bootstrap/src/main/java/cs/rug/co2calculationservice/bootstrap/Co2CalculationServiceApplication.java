83d17237cb35f02a4a695c83924ba8a8d59ca808
SSStoyanov <simosstoyanov@gmail.com>
2026-06-15 22:17:38 +0200

    CO2-Calculator: Adding emmission factors sql insert script + DDL in volume + SpringDataJpa  in bootstrap

--- co2-calculation-service/bootstrap/src/main/java/cs/rug/co2calculationservice/bootstrap/Co2CalculationServiceApplication.java
+++ co2-calculation-service/bootstrap/src/main/java/cs/rug/co2calculationservice/bootstrap/Co2CalculationServiceApplication.java
@@ -1,9 +1,13 @@
 package cs.rug.co2calculationservice.bootstrap;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "cs.rug.co2calculationservice")
+@AutoConfigurationPackage(basePackages = "cs.rug.co2calculationservice")
+@EnableJpaRepositories(basePackages = "cs.rug.co2calculationservice.infrastructure.persistence.repository")
 public class Co2CalculationServiceApplication {
 
     public static void main(String[] args) {
--- co2-calculation-service/bootstrap/src/main/resources/schema.sql
+++ co2-calculation-service/bootstrap/src/main/resources/schema.sql
@@ -19,44 +19,41 @@ CREATE TABLE IF NOT EXISTS emission_factors (
     CONSTRAINT uk_emission_factors_reference_set_fuel_type UNIQUE (reference_set_id, fuel_type)
 );
 
-CREATE OR REPLACE PROCEDURE seed_popescu_demo_reference_catalog()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    INSERT INTO resource_profiles (
-        reference_set_id,
-        name,
-        type,
-        fuel_per_use,
-        fuel_type,
-        fuel_unit,
-        time_unit
-    )
-    VALUES (
-        'popescu-demo-v1',
-        'Welder',
-        'atomic',
-        5.0,
-        'Diesel',
-        'l',
-        'hour'
-    )
-    ON CONFLICT (reference_set_id, name) DO NOTHING;
+INSERT INTO resource_profiles (
+    reference_set_id,
+    name,
+    type,
+    fuel_per_use,
+    fuel_type,
+    fuel_unit,
+    time_unit
+)
+VALUES (
+    'popescu-demo-v1',
+    'Welder',
+    'atomic',
+    5.0,
+    'Diesel',
+    'l',
+    'hour'
+)
+ON CONFLICT (reference_set_id, name) DO NOTHING;
 
-    INSERT INTO emission_factors (
-        reference_set_id,
-        fuel_type,
-        unit,
-        factor
-    )
-    VALUES (
-        'popescu-demo-v1',
-        'Diesel',
-        'l',
-        2.70553
-    )
-    ON CONFLICT (reference_set_id, fuel_type) DO NOTHING;
-END;
-$$;
-
-CALL seed_popescu_demo_reference_catalog();
+INSERT INTO emission_factors (
+    reference_set_id,
+    fuel_type,
+    unit,
+    factor
+)
+VALUES
+    ('popescu-demo-v1', 'Diesel', 'l', 2.70553),
+    ('popescu-demo-v1', 'Natural gas', 'm3', 2.02135),
+    ('popescu-demo-v1', 'Aviation turbine fuel', 'l', 2.54514),
+    ('popescu-demo-v1', 'Bioethanol', 'l', 0.00901),
+    ('popescu-demo-v1', 'Biodiesel ME', 'l', 0.16751),
+    ('popescu-demo-v1', 'Wood pellets', 't', 72.61754),
+    ('popescu-demo-v1', 'Grass/straw', 't', 49.23656),
+    ('popescu-demo-v1', 'Biogas', 't', 1.21518),
+    ('popescu-demo-v1', 'Energy_Romania', 'kwh', 0.2895),
+    ('popescu-demo-v1', 'Energy_Netherlands', 'kwh', 0.2029)
+ON CONFLICT (reference_set_id, fuel_type) DO NOTHING;
